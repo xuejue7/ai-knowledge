e2ef5b76ff63f24dffa6cfe54b7a504128caf861
xuejunke7 <xuejue702@gmail.com>
2026-09-14 23:19:41 +0800

    steam_api

---
+++ xue-dev-tech-app/src/main/java/cn/bugstack/airag/Application.java
@@ -0,0 +1,17 @@
+package cn.bugstack.airag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description AI RAG 知识库 应用启动入口
+ * @author xue
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
